790c4778734ad0af83b745afd2103fb23d1015cd
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2023-03-07 16:56:24 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.java
@@ -1,61 +1,30 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 class Solution {
     public int[] solution(int[] lottos, int[] win_nums) {
-        Map<Integer, Integer> win_nums_map = new HashMap<>();
-        int zero_count = 0;
-        int lotto_count = 0;
-        int min = 0;
-        int max = 0;
-        
-        for(int num: win_nums) {
-            win_nums_map.put(num, 0);
-        }
-        
-        for(int lotto: lottos) {
-            if(lotto == 0) zero_count++;
-            else {
-                if(win_nums_map.getOrDefault(lotto, -1) != -1) {
-                    lotto_count++;
-                    win_nums_map.remove(lotto);
-                }
+        Map<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+        int zeroCount = 0;
+
+        for(int lotto : lottos) {
+            if(lotto == 0) {
+                zeroCount++;
+                continue;
             }
+            map.put(lotto, true);
         }
-        
-        if(zero_count != 0) max = lotto_count + zero_count;
-        
-        switch(lotto_count) {
-            case 6: max = 1; min = 1;
-                break;
-            case 5: min = 2;
-                break;
-            case 4: min = 3;
-                break;
-            case 3: min = 4;
-                break;
-            case 2: min = 5;
-                break;
-            default: min = 6;
+
+
+        int sameCount = 0;
+        for(int winNum : win_nums) {
+            if(map.containsKey(winNum)) sameCount++;
         }
-        
-        if(zero_count == 0) max = min;
-        else {
-            switch(max) {
-                case 6: max = 1;
-                    break;
-                case 5: max = 2;
-                    break;
-                case 4: max = 3;
-                    break;
-                case 3: max = 4;
-                    break;
-                case 2: max = 5;
-                    break;
-                default: max = 6;
-            }
-        }
-        
-        int[] answer = {max, min}; 
-        return answer;      
+
+        int maxRank = 7 - (sameCount + zeroCount);
+        int minRank = 7 - sameCount;
+        if(maxRank > 6) maxRank = 6;
+        if(minRank > 6) minRank = 6;
+
+        return new int[] {maxRank, minRank};
     }
 }
