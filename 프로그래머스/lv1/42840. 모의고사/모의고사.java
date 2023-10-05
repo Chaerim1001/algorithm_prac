d347fdf0d06807cee58c54acde0d73ea4d4b4388
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2023-10-05 22:09:21 +0900

    [level 1] Title: 모의고사, Time: 0.76 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42840. 모의고사/모의고사.java
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -2,32 +2,37 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] supoja1 = {1,2,3,4,5}; // 5
-        int[] supoja2 = {2,1,2,3,2,4,2,5}; // 8
-        int[] supoja3 = {3,3,1,1,2,2,4,4,5,5}; // 10
-
-        List<Integer> count = new ArrayList<>(Arrays.asList(0,0,0,0));
-        List<Integer> answer = new ArrayList<>();
+        int[] one = {1,2,3,4,5};
+        int[] two = {2,1,2,3,2,4,2,5};
+        int[] three = {3,3,1,1,2,2,4,4,5,5};
         
-        for(int i=0; i< answers.length; i++) {
-            if(answers[i] == supoja1[i% supoja1.length]) count.set(1, count.get(1) + 1);
-            if(answers[i] == supoja2[i% supoja2.length]) count.set(2, count.get(2) + 1);
-            if(answers[i] == supoja3[i% supoja3.length]) count.set(3, count.get(3) + 1);
+        int oneCnt = countAnswer(one, answers);
+        int twoCnt = countAnswer(two, answers);
+        int threeCnt = countAnswer(three, answers);
+        
+        List<Integer> temp = new ArrayList<>();
+        int max = Math.max(oneCnt, Math.max(twoCnt, threeCnt));
+        
+        if(max == oneCnt) temp.add(1);
+        if(max == twoCnt) temp.add(2);
+        if(max == threeCnt) temp.add(3);
+        
+        int[] answer = new int[temp.size()];
+        for(int i=0; i<temp.size(); i++) {
+            answer[i] = temp.get(i);
         }
         
-        int max = Collections.max(count);
-        for(int i=1; i< count.size(); i++) {
-            if(count.get(i) == max) {
-                answer.add(i);
-            }
+        return answer;
+    }
+    
+    private int countAnswer(int[] personAnswer, int[] answers) {
+        int count = 0;
+        
+        if(personAnswer[0] == answers[0]) count++;
+        for(int i=1; i<answers.length; i++) {    
+            if(personAnswer[i%personAnswer.length] == answers[i]) count++;
         }
         
-        Collections.sort(answer);
-        int[] result = new int[answer.size()];
-        for(int i=0; i<answer.size(); i++) {
-            result[i] = answer.get(i);
-        }
-        
-        return result;
+        return count;
     }
 }
