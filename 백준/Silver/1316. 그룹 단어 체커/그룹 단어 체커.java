66a4e1dd4031b2b4782288bf9cc4e056889b62f3
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2023-10-07 15:04:39 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 124 ms, Memory: 14368 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,36 @@
+import java.io.*;
+
+public class Main {
+    // a - 97
+    // 알파벳 총 26개
+    static int answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        answer = 0;
+        
+        for(int i=0; i<num; i++) {
+            checkGroupWord(br.readLine());
+        }
+        
+        System.out.println(answer);
+        br.close();
+    }
+    
+    private static void checkGroupWord(String word) {
+        int[] a = new int[26];
+        char before = word.charAt(0);
+        a[before-97]++;
+        for(int i=1; i<word.length(); i++) {
+            char c = word.charAt(i);
+            if(before!=c) {
+                if(a[c-97]>0) return;
+                else {
+                    a[c-97]++;
+                    before = c;
+                }
+            }else a[before-97]++;
+        }
+        answer++;
+    }
+}
