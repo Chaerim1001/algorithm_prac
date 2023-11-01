007610ecaf28d80f30a143270032e17334e60f53
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2023-11-01 21:17:47 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,30 +1,17 @@
 class Solution {
     public int solution(int storey) {
-        char[] charArr = (storey + "").toCharArray();
-        int[] intArr = new int[charArr.length];
         int answer = 0;
-        
-        for(int i=0; i<charArr.length; i++) {
-            intArr[i] = Character.getNumericValue(charArr[i]);
-        }
-        
-        for(int i=charArr.length-1; i>=0; i--) {
-            int plus = 0;
-            if(intArr[i]>5) {
-                if(i==0) plus = 10-intArr[i]+1;
-                else {
-                    intArr[i-1] += 1;
-                    plus = 10-intArr[i];
-                }
-            } else if(intArr[i]==5 && i!=0) {
-                if(intArr[i-1]+1>5) intArr[i-1] += 1;
-                plus = 5;
+        while (storey != 0) {
+            int upperNumber = (storey %100)/10;
+            int number = storey % 10;
+            if (number > 5 || number == 5 && upperNumber>=5) {
+                storey += 10;
+                answer += (10 - number);
             } else {
-                plus = intArr[i]; 
+                answer += number;
             }
-            answer += plus;
-        }
-        
+            storey = storey / 10;
+        }  
         return answer;
     }
 }
