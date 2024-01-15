3e2dfdbc371f20414a01922eeb7f2f0f8dd3739b
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-01-15 13:15:01 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 268 ms, Memory: 16860 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+     
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=n; i++) {
+            int idx = n;
+            while(idx>i) {
+                System.out.print(" ");
+                idx--;
+            }
+            for(int j=0; j<i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        br.close();
+    }
+}
