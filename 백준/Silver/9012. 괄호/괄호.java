3c42248a60493ceee7799356f419c89cc6693e4d
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-01-18 15:48:34 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14284 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -4,40 +4,29 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        
-        for(int i=0; i<N; i++) {
-            String target = br.readLine();
-            if(target.length() % 2 != 0) {
-                System.out.println("NO");
-                continue;
-            } 
-            isVPS(target);
+        int cases = Integer.parseInt(br.readLine());
+
+        while(cases-->0) {
+            char[] target = br.readLine().toCharArray();
+            Stack<Character> stack = new Stack<>();
+            boolean result = true;
+
+            for(char c: target) {
+                if(c=='(') stack.push(c);
+                else {
+                    if(stack.isEmpty()) {
+                        result = false;
+                        break;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+
+            if(stack.isEmpty() && result) System.out.println("YES");
+            else System.out.println("NO");
         }
         
         br.close();
-    }
-    
-    private static void isVPS(String target) {
-        Stack<Character> stack = new Stack<>();
-        for(char c: target.toCharArray()) {
-            stack.push(c);
-        }
-        
-        int n = 0;
-        while(!stack.isEmpty()) {
-            char c = stack.peek();
-            if(c=='(') {
-                if(n<=0) break;
-                stack.pop();
-                n--;
-            } else {
-                stack.pop();
-                n++;
-            }
-        }
-        
-        if(n!=0 || !stack.isEmpty()) System.out.println("NO");
-        else System.out.println("YES");
     }
 }
