0e14b946f31518fe22b4d1e0ee59c1e962678d86
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-01-23 16:57:45 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 124 ms, Memory: 14040 KB -BaekjoonHub

--- 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.java
@@ -1,26 +1,24 @@
 import java.io.*;
-import java.util.*;
-
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-        int[][] apartment = new int[15][15];
-
-        for(int i=1; i<15; i++) {
-            apartment[0][i] = i;
-        }
-
-        for(int i=1; i<15; i++) {
-            for(int j=1; j<15; j++) {
-                apartment[i][j] = apartment[i-1][j] + apartment[i][j-1];
-            }
-        }
 
         for(int  i=0; i<T; i++) {
             int k = Integer.parseInt(br.readLine());
             int n = Integer.parseInt(br.readLine());
+            int[][] apartment = new int[k+1][n+1];
+            
+            for(int j=1; j<n+1; j++) {
+                apartment[0][j] = j;
+            }
+
+            for(int j=1; j<k+1; j++) {
+                for(int l=1; l<n+1; l++) {
+                    apartment[j][l] = apartment[j-1][l] + apartment[j][l-1];
+                }
+            }
 
             System.out.println(apartment[k][n]);
         }
