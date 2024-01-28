54cc6ebbd26bc8ca9b2c95a726b1f2fc5718d697
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-01-29 00:44:17 +0900

    [Silver III] Title: 통계학, Time: 612 ms, Memory: 49616 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -4,59 +4,55 @@ import java.util.*;
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		List<Integer> list = new ArrayList<>();
-		HashMap<Integer, Integer> hashmap = new HashMap<>();
+		int N = Integer.parseInt(br.readLine());
+        int mode = 0; // 최빈값
 
-		int loop = Integer.parseInt(br.readLine());
+		int[] arr = new int[N];
+		int[] positive_arr = new int[4001];
+		int[] negative_arr = new int[4001];
+
 		double sum = 0;
-		for(int i=0; i<loop; i++) {
+
+		for(int i=0; i<N; i++) {
 			int temp = Integer.parseInt(br.readLine());
-			sum += (double) temp;
-			list.add(temp);
-		}
+			
+			// 평균 계산을 위한 총합 계산
+			sum += temp;
+			
+			// 중앙값 및 범위를 위한 배열에 삽입
+			arr[i] = temp;
 
-		Collections.sort(list);
+			// 최빈값을 구하기 위함
+			if(temp>=0) positive_arr[temp]++;
+			else negative_arr[Math.abs(temp)]++;
+        }
 
-		int max = Collections.max(list);
-		int min = Collections.min(list);
+		// 정렬
+		Arrays.sort(arr);
 
-		if(max < 0 && min < 0) min = Math.abs(min);
-		else if(max > 0 && min > 0) min = min*-1;
-		else {
-			max = Math.abs(max);
-			min = Math.abs(min);
-		}
+        int max = 0;
+        for(int i=0; i<positive_arr.length; i++) {
+            max = Math.max(max, positive_arr[i]);
+        }
+        for(int i=0; i<negative_arr.length; i++) {
+            max = Math.max(max, negative_arr[i]);
+        }
 
-		double dle = sum / loop;
-		int average = (int) Math.round(dle);
-		int median = list.get(loop/2);
-		int mode = 0;
-		int range = max + min;
+        List<Integer> list = new ArrayList<>();
+        for(int number: arr) {
+            if(number<0) {
+                if(negative_arr[Math.abs(number)] == max && !(list.contains(number))) list.add(number);
+            } else {
+                if(positive_arr[number] == max && !(list.contains(number))) list.add(number);
+            }
+        }
 
-		for(int num : list) {
-			hashmap.put(num, hashmap.getOrDefault(num, 0) + 1);
-		}
+        if(list.size() >= 2) mode = list.get(1);
+        else mode = list.get(0);
 
-		int numbers = 0;
-		for(int key : hashmap.values()) {
-			numbers = Math.max(numbers, key);
-		}
-
-		list.clear();
-		for(int key : hashmap.keySet()) {
-			if(hashmap.get(key) == numbers) {
-				list.add(key);
-			}
-		}
-
-		Collections.sort(list);
-
-		if(list.size() >= 2) mode = list.get(1);
-		else mode = list.get(0);
-		
-		System.out.println(average);
-		System.out.println(median);
-		System.out.println(mode);
-		System.out.println(range);
+        System.out.println(Math.round(sum/N));
+        System.out.println(arr[N/2]);
+        System.out.println(mode);
+        System.out.println(arr[N-1]-arr[0]);
 	}
 }
