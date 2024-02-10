a12b3856184cbeb0e84205947be833dd6fc02060
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-02-10 18:41:57 +0900

    [Silver II] Title: 랜선 자르기, Time: 232 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static List<Long> list;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        
+        list = new ArrayList<>();
+        long max = 0;
+        for(int i=0; i<K; i++) {
+            long length = Long.parseLong(br.readLine());
+            list.add(length);
+            max = Math.max(max, length);
+        }
+        
+        max++;
+        long min = 0;
+        long mid = 0;
+         
+        while(min<max) {
+            mid = (min+max)/2;
+            long count = 0;
+            for(long n: list) {
+                count += (n / mid);
+            }
+        
+            if(count < N) {
+                max = mid;
+            } else {
+                min = mid + 1;
+            }
+        }
+        
+        System.out.println(max-1);
+        br.close();
+    }
+}
