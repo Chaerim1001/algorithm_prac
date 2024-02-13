358cd8d0617b56aa91228f692a7d74fadc1c0872
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-02-13 16:49:47 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14252 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,8 +1,8 @@
 import java.io.*;
 
 public class Main {
-    static final int R = 31;
     static final int M = 1_234_567_891;
+    static final int R = 31;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -14,11 +14,14 @@ public class Main {
     }
     
     static long hash(int length, String str) {
-        char[] arr = str.toCharArray();
         long sum = 0;
+        long k = 1;
+        
+        char[] arr = str.toCharArray();
         for(int i=0; i<length; i++) {
             int n = arr[i] - 96;
-            sum += (n* Math.pow(R, i));
+            sum += (n*k)%M;
+            k = (k*R)%M;
         }
         
         return sum%M;
