8fef4e6c1dfea0e9edf74ad338ac0b02c678a2b5
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-03-21 23:59:30 +0900

    [Gold IV] Title: 거짓말, Time: 128 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,92 +1,82 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
-
-    static int[] parent;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] inputs = br.readLine().split(" ");
-
-        int n = Integer.parseInt(inputs[0]);
-        int m = Integer.parseInt(inputs[1]);
-
-        boolean[] people_know = new boolean[n+1]; 
-
-        HashSet<Integer>[] parties = new HashSet[m+1];
-        for (int i = 1; i <= m; i++) {
-            parties[i] = new HashSet<>();
-        }
-
-        inputs = br.readLine().split(" ");
-        int know_num = Integer.parseInt(inputs[0]);
-
-        for (int i = 1; i <= know_num; i++) {
-            int tmp = Integer.parseInt(inputs[i]);
-            people_know[tmp] = true;
-        }
-
-        parent = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            parent[i] = i;
-        }
-
-        for (int p = 1; p <= m; p++) { 
-            inputs = br.readLine().split(" ");
-            int party_num = Integer.parseInt(inputs[0]); 
-
-            if(party_num<=1) {
-                parties[p].add(Integer.parseInt(inputs[1]));
-                continue;
-            }
-
-            for (int j = 1; j < party_num; j++) { 
-                int a = Integer.parseInt(inputs[j]);
-                int b = Integer.parseInt(inputs[j+1]);
-                if (find(a) != find(b)) union(a,b);
-
-                parties[p].add(a); 
-                parties[p].add(b);
-            }
-        }
-
-        boolean[] visited = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
-            if(people_know[i] && !visited[i]){
-                int root = find(i);
-                for (int j = 1; j <= n; j++){
-                    if (find(j)==root) {
-                        people_know[j] = true;
-                        visited[j] = true;
-                    }
-                }
-            }
-        }
-
-        int result = 0;
-        for (int i = 1; i <= m; i++) { 
-            boolean flag = false;
-            for (int person : parties[i]) { 
-                if(people_know[person]){
-                    flag = true;
+	static int[] parents;
+	static List<Integer> eList;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		parents = new int[n+1];
+		for(int i=1; i<n+1; i++) {
+			parents[i] = i;
+		}
+		st = new StringTokenizer(br.readLine());
+		int en= Integer.parseInt(st.nextToken());
+		eList = new ArrayList<>();
+		if(en==0) {
+			System.out.println(m);
+			return;
+		}
+		else{
+			for(int i=0; i<en; i++) {
+				eList.add(Integer.parseInt(st.nextToken()));
+			}
+		}
+		
+		List<Integer>[] partyList = new ArrayList[m];
+		for(int i=0; i<m; i++) {
+			partyList[i] = new ArrayList<>();
+		}
+		
+		for(int i=0; i<m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int pn = Integer.parseInt(st.nextToken());
+			
+			int x = Integer.parseInt(st.nextToken());
+			partyList[i].add(x);
+			for(int j=1; j<pn; j++) {
+				int y = Integer.parseInt(st.nextToken());
+				union(x,y);
+				partyList[i].add(y);
+			}
+		}
+        
+		int cnt=0;
+		for(int i=0; i<m; i++) {
+			boolean flag = true;
+			for(int num : partyList[i]) {
+				if(eList.contains(find(parents[num]))) {
+                    flag= false;
                     break;
-                }
+    			}
             }
-            if(!flag) result++;
-        }
-        System.out.println(result);
-    }
-
-    public static int find(int idx) {
-        if(parent[idx]==idx){
-            return idx;
-        }
-        parent[idx] = find(parent[idx]);
-        return parent[idx];
-    }
-
-    public static void union(int a, int b) {
-        int parent_b = find(b);
-        parent[parent_b] = a; 
-    }
+			if(flag) {
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+		
+	}
+	
+	static int find(int x) {
+		if(parents[x] ==x ) return x;
+		return find(parents[x]);
+	}
+	
+	static void union(int x, int y) {
+		int rx = find(x);
+		int ry = find(y);
+		if(eList.contains(ry)) {
+			int tmp = rx;
+			rx = ry;
+			ry =tmp;
+		}
+		
+		parents[ry] = rx;
+	}
 }
