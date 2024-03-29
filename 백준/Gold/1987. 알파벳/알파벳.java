d755dc31c87e1456a3b1a617fa0acf9aa6463d82
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-03-30 04:10:07 +0900

    [Gold IV] Title: 알파벳, Time: 848 ms, Memory: 15448 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,49 +1,52 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    
     static int R, C;
-    static int[] dx = { -1, 1, 0, 0 };
-    static int[] dy = { 0, 0, -1, 1 };
     static int[][] map;
-    static boolean[] visited = new boolean[26];
+    static boolean[] alpha;
+    
     static int answer = 0;
     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
         R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
         map = new int[R][C];
+        alpha = new boolean[26];
         
-        for(int i = 0; i < R; i++) {
+        for(int i=0; i<R; i++) {
             String str = br.readLine();
-            for(int j = 0; j < C; j++) {
+            for(int j=0; j<C; j++) {
                 map[i][j] = str.charAt(j) - 'A';
             }
+ 
         }
         
-        dfs(0,0,0);
+        dfs(0, 0, 1);
+        System.out.println(answer);
+        
         br.close();
-        System.out.print(answer);
     }
     
-    static void dfs(int x, int y, int count) {
-        if(visited[map[x][y]]) {
-            answer = Math.max(answer, count);
-            return;
-        } else {
-            visited[map[x][y]] = true;
-            for(int i = 0; i < 4; i++) {
-                int cx = x + dx[i];
-                int cy = y + dy[i];
-                
-                if(cx>=0 && cy>=00 && cx < R && cy < C) {
-                    dfs(cx, cy, count+1);
-                }
+    static void dfs(int x, int y, int length) {
+        alpha[map[x][y]] = true;
+        answer = Math.max(length, answer);
+        
+        for(int i=0; i<4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            if(nx<0 || ny<0 || nx>=R ||ny>=C) continue;
+            
+            if(!alpha[map[nx][ny]]) {
+                dfs(nx, ny, length+1);
+                alpha[map[nx][ny]] = false;
             }
-            visited[map[x][y]] = false;
         }
     }
 }
