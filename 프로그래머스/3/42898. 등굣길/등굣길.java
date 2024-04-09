cb7d619aaa9c7463ccbbcf5ecea8dd51bad63dab
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-04-09 22:16:50 +0900

    [level 3] Title: 등굣길, Time: 0.43 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,49 +1,42 @@
-import java.util.*;
-
 class Solution {
-    static final int MOD = 1_000_000_007;
-    static int[] dx = {1, 0};
-    static int[] dy = {0, 1};
     
     public int solution(int m, int n, int[][] puddles) {
         int[][] dp = new int[n+1][m+1];
+        // dp[i][j] = dp[i-1][j] + dp[i][j-1];
         
-        for(int i=1; i<m+1; i++) {
+        final int MOD = 1_000_000_007;
+        
+        for(int i=1; i<=m; i++) {
             dp[1][i] = 1;
         }
         
-        for(int i=1; i<n+1; i++) {
+        for(int i=1; i<=n; i++) {
             dp[i][1] = 1;
         }
         
-        for(int[] p: puddles) {
-            dp[p[1]][p[0]] = -1;
-            
-            if(p[1] == 1) {
-                for(int i=p[0]+1; i<m+1; i++) {
-                    dp[1][i] = -1;
+        for(int[] p:puddles) {
+            if(p[0] == 1) {
+                for(int i=p[1]; i<=n; i++) {
+                    dp[i][1] = 0;
                 }
-            } else if(p[0] == 1) {
-                for(int i=p[1]+1; i<n+1; i++) {
-                    dp[i][1] = -1;
+            } else if(p[1] == 1) {
+                for(int i=p[0]; i<=m; i++) {
+                    dp[1][i] = 0;
                 }
+            } else {
+                dp[p[1]][p[0]] = -1;
             }
         }
         
-        for(int i=2; i<n+1; i++) {
-            for(int j=2; j<m+1; j++) {
-                if(dp[i][j] != -1) {
-                    if(dp[i-1][j] == -1 && dp[i][j-1] != -1) {
-                        dp[i][j] = dp[i][j-1] % MOD;
-                    } else if(dp[i-1][j] != -1 && dp[i][j-1] == -1) {
-                        dp[i][j] = dp[i-1][j] % MOD;
-                    } else if(dp[i-1][j] != -1 && dp[i][j-1] != -1) {
-                        dp[i][j] = (dp[i-1][j] +dp[i][j-1]) % MOD;
-                    }
-                }
+        for(int i=2;i<=n;i++) {
+            for(int j=2; j<=m; j++) {
+                if(dp[i][j] == -1) {
+                    dp[i][j] = 0;
+                    continue;
+                } 
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1])%MOD;
             }
         }
-        
-        return dp[n][m];
+        return dp[n][m]%MOD;
     }
 }
