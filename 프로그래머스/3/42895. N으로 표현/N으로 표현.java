7a4614d6ff871b9d3143284f2411cadbc5d9f377
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-04-14 16:20:47 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -3,7 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int N, int number) {
         if (N == number) return 1;
- 
+        
         List<Set<Integer>> dp = new ArrayList<>();
         for (int i = 0; i <= 8; i++) {
             dp.add(new HashSet<>());
@@ -11,11 +11,13 @@ class Solution {
  
         dp.get(1).add(N);
 
-        for (int i = 2; i <= 8; i++) {
+        for (int i=2; i<=8; i++) {
             StringBuilder sb = new StringBuilder().append(N);
+            
             for (int j = 1; j < i; j++) {
                 sb.append(N);
             }
+            
             dp.get(i).add(Integer.parseInt(sb.toString()));
  
             for (int j = 1; j < i; j++) {
@@ -26,13 +28,11 @@ class Solution {
                         dp.get(i).add(num1 - num2);
                         dp.get(i).add(num1 * num2);
                         if (num2 != 0) dp.get(i).add(num1 / num2);
-                        
                     }
                 }
             }
  
-            if (dp.get(i).contains(number)) return i;
-            
+            if (dp.get(i).contains(number)) return i;   
         }
 
         return -1;
