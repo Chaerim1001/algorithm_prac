c10344049a074d8ada99e6bbfbd7829d5d988ba1
Chaerim1001 <89819254+Chaerim1001@users.noreply.github.com>
2024-04-27 16:37:02 +0900

    [Gold IV] Title: 문자열 폭발, Time: 516 ms, Memory: 34232 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -8,32 +8,36 @@ public class Main {
         String target = br.readLine();  
 
         Stack<Character> st = new Stack<>();
+        
         for(int i=0; i<str.length(); i++) {
-			st.push(str.charAt(i));
-			
-			if(st.size()>= target.length()) {
-				boolean flag = true;
-				
-				for(int j=0; j<target.length(); j++) {
-					if(st.get(st.size()-target.length()+j) != target.charAt(j)) {
-						flag = false;
-						break;
-					}
-				}
-				if(flag) {
-					for(int j=0; j<target.length(); j++) {
-						st.pop();
-					}
-				}
-			}
-			
-		}
+            st.push(str.charAt(i));
+
+            if(st.size()>=target.length()) {
+                boolean flag = true;
+                for(int j=0; j<target.length(); j++) {
+                    if(target.charAt(j) != st.get(st.size()-target.length()+j)) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if(flag) {
+                    for(int k=0; k<target.length(); k++) {
+                        st.pop();
+                    }
+                }
+            }
+        }
 		
 		StringBuilder sb = new StringBuilder();
-		for(Character c : st) {
-			sb.append(c);
-		}
-		System.out.println(sb.length()==0? "FRULA" : sb.toString());
+        if(st.isEmpty()) {
+            sb.append("FRULA");
+        } else {
+            for(char c: st) {
+                sb.append(c);
+            }
+        }
+
+		System.out.println(sb.toString());
         br.close();
     }
 }
